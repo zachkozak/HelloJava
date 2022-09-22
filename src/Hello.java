9dad01b0718c9bb112c854240c9877450c25ebb1
zachkozak <111797839+zachkozak@users.noreply.github.com>
2022-09-22 11:32:00 -0500

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author Zach Kozak
+ * 
+ * Git @zachkozak
+ * 2022/09/22
+ *
+ */
+public class Hello {
+	public static void main(String args[]) {
+		System.out.println("Hello World! -Zach Kozak");
+		System.out.println("(Computer Science Major)");
+		
+	}
+}
